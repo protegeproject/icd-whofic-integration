df2f6f7a40b76a89b8054b02baf8b773723664c0
Tania Tudorache <tudorache@users.noreply.github.com>
2020-06-19 12:10:06 +0000

    adding also the first two levels for the groupings

--- src/main/java/org/who/integration/util/KBUtil.java
+++ src/main/java/org/who/integration/util/KBUtil.java
@@ -4,20 +4,37 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import edu.stanford.bmir.whofic.icd.ICDContentModel;
 import edu.stanford.smi.protegex.owl.model.RDFSNamedClass;
 
 public class KBUtil {
-
+	
 	public static Collection<RDFSNamedClass> getNamedSubclasses(RDFSNamedClass cls) {
+		return getNamedSubclasses(cls, false);
+	}
+
+	public static Collection<RDFSNamedClass> getNamedSubclasses(RDFSNamedClass cls, boolean transitive) {
 		List<RDFSNamedClass> namedSubclses = new ArrayList<RDFSNamedClass>();
 		
-		for (Object subcls : cls.getSubclasses(false)) {
+		for (Object subcls : cls.getSubclasses(transitive)) {
 			if (subcls instanceof RDFSNamedClass) {
 				namedSubclses.add((RDFSNamedClass) subcls);
 			}
 		}
 		
 		return namedSubclses;
+	}
+	
+	public static Collection<String> getTitles(ICDContentModel cm, Collection<RDFSNamedClass> clses) {
+		Collection<String> list = new ArrayList<String>();
+		
+		for (RDFSNamedClass cls : clses) {
+			String title = cm.getTitleLabel(cls);
+			title = title == null ? cls.getName() : title;
+			list.add(title);
+		}
+		
+		return list;
 	}
 	
 	
